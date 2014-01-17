d47ea31fcef001d97cb5b66e11d3829014dff6b0
Mark Small <m.small@ed.ac.uk>
2014-01-17 10:57:05 +0000

    Added dummy bashrc for Andrew Seales.

--- env/setup.sh
+++ env/setup.sh
@@ -3,6 +3,7 @@
 rm -rf $HOME/.profile\
 		   $HOME/.bashrc \
        $HOME/.bashrc_mark-small \
+       $HOME/.bashrc_as\
        $HOME/.emacs \
        $HOME/.emacs.d \
        $HOME/.gitconfig \
@@ -17,6 +18,7 @@ rm -rf $HOME/.profile\
 ln -s $HOME/.environment/env/profile $HOME/.profile
 ln -s $HOME/.environment/env/bashrc $HOME/.bashrc
 ln -s $HOME/.environment/env/bashrc_mark-small $HOME/.bashrc_mark-small
+ln -s $HOME/.environment/env/bashrc_as $HOME/.bashrc_as
 ln -s $HOME/.environment/env/emacs $HOME/.emacs
 ln -s $HOME/.environment/env/emacs.d $HOME/.emacs.d
 ln -s $HOME/.environment/env/gitconfig $HOME/.gitconfig
